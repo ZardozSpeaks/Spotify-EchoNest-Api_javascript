f173a62d102a1879be8c418fdf239069c0d8b678
alissa paluska and david remington <david.ian.remington@gmail.com>
2016-03-16 15:24:44 -0700

    map JSON object on local object

--- js/artist-interface.js
+++ js/artist-interface.js
@@ -9,21 +9,22 @@ $(document).ready(function() {
   $('#enter').click(function() {
     var artistName = $('#artistName').val();
     $('#artistName').val("");
-    var newartist = new artist.Artist(artistName);
+    var newArtist = new artist.Artist();
     findSpotifyId();
 
     function findSpotifyId() {
       $.get('https://api.spotify.com/v1/search?q=' + artistName + '&type=artist').then(function(response) {
         //display reponse on page
         console.log(response);
-        artistId = response.artists.items[0].id;
-        console.log("artistId:" + artistId);
+        artist = response.artists.items[0];
         findSimilarArtists();
+        $.extend( newArtist, artist );
+        console.log(newArtist)
        });
      }
 
     function findSimilarArtists() {
-    $.get('http://developer.echonest.com/api/v4/artist/similar?api_key=' + echoNestKey +  '&id=spotify:artist:' + artistId + '&bucket=id:spotify').then(function(response) {
+    $.get('http://developer.echonest.com/api/v4/artist/similar?api_key=' + echoNestKey +  '&id=spotify:artist:' + artist.id + '&bucket=id:spotify').then(function(response) {
       //display reponse on page
       console.log(response);
     });
--- js/artist.js
+++ js/artist.js
@@ -1,28 +1,21 @@
-exports.Artist = function(artistName) {
-    this.artistName = artistName;
+exports.Artist = function() {
+    this.name = "";
     this.genres = [];
     this.popularity = 0;
-    this.artistHref = "";
-    this.artistImages = [];
-};
-
-exports.Artist.prototype.matchGenre = function(Artist) {
-    function isSame(genre) {
-        return this.Artist.genre === artist.getArtist();
-    }
-    Artist = [Artist.genres].filter(isSame);
+    this.href = "";
+    this.images = [];
 };
 
 exports.Artist.prototype.addImages = function(images) {
-    this.artistImages.push(images);
+    this.images.push(images);
 };
 
 exports.Artist.prototype.getArtist = function() {
     var artistInfo = [];
-    artistInfo.push(this.artistName);
+    artistInfo.push(this.name);
     artistInfo.push(this.genres);
     artistInfo.push(this.popularity);
-    artistInfo.push(this.artistHref);
-    artistInfo.push(this.artistImages);
+    artistInfo.push(this.href);
+    artistInfo.push(this.images);
     return artistInfo;
 };
