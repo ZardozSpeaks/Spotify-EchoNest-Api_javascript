a0c205c369a85b3c9d2adfbf88df0e52b5f46eeb
David Remington <david.ian.remington@gmail.com>
2016-03-17 16:47:59 -0700

    refactor code for simplicity

--- js/artist-interface.js
+++ js/artist-interface.js
@@ -1,4 +1,3 @@
-var artist = require('./../js/artist.js');
 var spotifyKey = require('./../.env').spotifyKey;
 var echoNestKey = require('./../.env').echoNestKey;
 
@@ -7,26 +6,29 @@ $(document).ready(function() {
   $('#enter').click(function() {
     $('.band-group').remove();
     var artistName = $('#artistName').val();
-    var newArtist = new artist.Artist();
-    findSpotifyId();
+    findArtist();
 
-    function findSpotifyId() {
+    function findArtist() {
       $.get('https://api.spotify.com/v1/search?q=' + artistName + '&type=artist').then(function(response) {
         console.log(response);
         var artist = response.artists.items[0];
-        $.extend( true, newArtist, artist );
-        $('#top-text').html('<h2>Artists similar to ' + JSON.stringify(newArtist.name) +'</h2>' )
-        findSimilarArtists();
-        console.log(newArtist);
+        var url = artist.external_urls.spotify;
+        $('#top-text').html('<h2>Artists similar to <a href=\"' + url +'\" target=\"_blank\">' +artist.name +'</a></h2>' )
+        var spotifyId = artist.id;
+        findSimilarArtists(spotifyId);
       });
     }
 
-    function findSimilarArtists() {
+    function findSimilarArtists(spotifyId) {
       var similarIds = [];
-      $.get('http://developer.echonest.com/api/v4/artist/similar?api_key=' + echoNestKey +  '&id=spotify:artist:' + newArtist.id + '&bucket=id:spotify').then(function(response) {
+      $.get('http://developer.echonest.com/api/v4/artist/similar?api_key=' + echoNestKey +  '&id=spotify:artist:' + spotifyId + '&bucket=id:spotify').then(function(response) {
         console.log(response);
         for (var i = 0; i < response.response.artists.length; i++) {
-          var id = response.response.artists[i].foreign_ids[0].foreign_id;
+        if (response.response.artists[i].hasOwnProperty("foreign_ids")) {
+            var id = response.response.artists[i].foreign_ids[0].foreign_id;
+          } else {
+            var id = "spotify:artist:4gzpq5DPGxSnKTe4SA8HAU"
+          }
           var idSlice = id.slice(15);
           similarIds.push(idSlice);
           idString = similarIds.join(',');
@@ -42,7 +44,8 @@ $(document).ready(function() {
         for (var i = 0; i < response.artists.length; i++) {
           var name = response.artists[i].name;
           var pic = response.artists[i].images[0].url;
-          $('#results').append('<div class=\"band-group\"><h3>' + name + '</h3>' + '<img src=' + pic + '></div>');
+          var link = response.artists[i].external_urls.spotify;
+          $('#results').append('<div class=\"band-group\"><h3><a href=\"' +link + '\" target=\"_blank\">' + name + '</a></h3>' + '<img src=' + pic + '></div>');
         }
       });
     }
